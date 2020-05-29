41ec27daba960328472440c3700cf7e934050361
정광현 <wjdrhkd456@gmail.com>
2020-05-29 13:30:03 +0900

    4일차(오후) 13:29

--- src/main/java/kr/re/kitri/hello/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/re/kitri/hello/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package kr.re.kitri.hello.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLException;
 import java.sql.SQLInvalidAuthorizationSpecException;
@@ -12,8 +12,9 @@ import java.sql.SQLInvalidAuthorizationSpecException;
 * 코드번호로 쉽게 처리할 수 있는 정책을 만드는 것이 좋다.
 * */
 
-@ControllerAdvice
-@RestController // 일종의 컨트롤러 역할을 하는 것이다.
+//@ControllerAdvice
+//@RestController // 일종의 컨트롤러 역할을 하는 것이다.
+@RestControllerAdvice//(@ControllerAdvice + @RestController)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = SQLInvalidAuthorizationSpecException.class)
@@ -31,9 +32,17 @@ public class GlobalExceptionHandler {
         return "SQL 예외 발생";
     }
 
+    // BadSqlGrammerException 이 sql 라이브러리안에 있는 SyntaxException 을 wrapping 하기 때문에
+    // 늘 이를 잘 생각해야한다.
+    @ExceptionHandler(value = BadSqlGrammarException.class)
+    public String handleBadSQLGException()
+    {
+        return "SQL 예외 발생";
+    }
+
     @ExceptionHandler(value = Exception.class)
-    public String handleAll() {
-        return "기타등등 예외를 모두 여기서 잡아버립니다.";
+    public String handleAll(Exception e) {
+        return "기타등등 예외를 모두 여기서 잡아버립니다." + e;
     }
 
 }
